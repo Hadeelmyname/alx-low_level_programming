b176ee7bcd2904cac83840b769992437505e277d
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-07 16:52:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * this is a puts function to print out a senentence followe by a new line
  * main is the entery point
- * retun the value 0
+ * retun the value 0 is (Success)
 */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
