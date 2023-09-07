e7266bc8dfdca41d368072be0706b8fcd0410e69
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-07 16:31:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * this is a puts function to print out a senentance
+ * main is the entery point
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts( "\"Programming is like building a multilingual puzzle" );
 
 return(0);
 }
