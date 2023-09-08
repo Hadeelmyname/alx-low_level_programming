f13423cf6e057f81dd0bf56c5e5292041c39cee5
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-07 17:04:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * this is a puts function to print out a senentence followe by a new line
  * main is the entery point
+ *
  * retun the value 0 is (Success)
 */
 int main(void)
