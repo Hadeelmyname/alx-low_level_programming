98a9db7608de2d5176a486c9ec5bef51423ba752
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-10 09:12:47 +0000

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints single digits of based 10 from 0
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
