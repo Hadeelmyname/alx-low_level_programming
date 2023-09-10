97278b7437b697ab78691bc340f14c3ad9113f57
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-10 12:24:52 +0000

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combination of a digit
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int v;
+
+	for (v = 0; v <= 9; v++)
+	{
+	putchar(v);
+	if (v != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	return (0);
+}
