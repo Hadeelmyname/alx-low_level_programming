c09d30a55fbb99da11db0c6324c60384137621cf
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-10 13:45:06 +0000

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,11 +9,9 @@ int main(void)
 	int a, b, c;
 
 	for (a = '0'; a < '9'; a++)
-	
 	{
 	for (b = a + 1; b <= '9'; b++)
 	{
-
 	for (c = b + 1; c <= '9'; c++)
 	{
 	if ((b != a) != c)
@@ -21,7 +19,7 @@ int main(void)
 	putchar(a);
 	putchar(b);
 	putchar(c);
-	if (a =='7' && b == '8')
+	if (a == '7' && b == '8')
 	continue;
 	putchar(',');
 	putchar(' ');
