50168b70159d3bab76c6b341b5bc971df1a3ad8c
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-10 11:52:57 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print number in based 16
+ *
+ * Return: 0 Always (Success)
+ *
+ */
+int main(void)
+{
+	int s;
+
+	for (s = 0; s <= 9; s++)
+	putchar(s * 10);
+	putchar('\n');
+	return (0);
+}
