611cc53525018f7cdb0c6f35ad0e756d4511646c
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-12 15:09:42 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Make alphabet_x10
+ *
+ * Return: void 
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int num = 1;
+
+	while (num <= 10)
+	{
+	num++;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+	_putchar (alpha);
+	}
+	_putchar ('\n');
+	}
+}
