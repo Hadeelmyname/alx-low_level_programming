59a7a67e4ad43e4ef1e1f08ca6bababb62f46a62
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-14 14:22:16 +0000

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Prints if the number is positive or negative
+ *
+ * Return 0 Always (Success)
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+return (0);
+}
