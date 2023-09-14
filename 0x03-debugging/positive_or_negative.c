3e7abdfe25e0b94f9f9dd36bc8794ccb844c7594
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-14 16:35:50 +0000

    2-largest_number.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * main - Entry point
+ * Descreption: 'the program will check a number is it positive or negative'
+ * @i: the number to be tested
+ * Return: Always 0 (Success)
+ */
 void positive_or_negative(int i)
 {
 	if (i > 0)
