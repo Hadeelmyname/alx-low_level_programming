6a863f5244a3df78a6ba5e570c5334f995d5da47
Hadeelmyname <hadeelawad62@gmail.com>
2023-09-18 23:10:38 +0000

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,10 +7,10 @@
 
 int _strlen(char *s)
 {
-	int leng;
-	int count = 0;
+int leng;
+int count = 0;
 
 for (leng = 0; s[leng] != '\0'; leng++)
-	       count++;
-	return (count);
+count++;
+return (count);
 }
