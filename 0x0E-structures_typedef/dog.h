6ac7618ebaa77e2d85980c0949e4dc08c380d977
Hadeelmyname <hadeelawad62@gmail.com>
2023-10-11 14:05:17 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct to create a dog info
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: first struct on prgram
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
